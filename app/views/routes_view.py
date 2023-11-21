dff9910252fddf4c238074ccf8209cd25427aa0c
Vinicius Bignardi <vinicius.bignardi@outlook.com>
2023-11-21 17:53:23 -0300

    estruturação mvc

--- app/views/routes_view.py
+++ app/views/routes_view.py
@@ -1,11 +1,9 @@
-from flask import Flask, request, jsonify, send_file
-#from app.controllers.qr_controller import QRController
+from flask import request, send_file
 
+from app import app
 from app.controllers import qr_controller
 
 qr_ctr = qr_controller.QRController()
-
-app = Flask(__name__)
 qr_controller = qr_ctr
 
 @app.route('/generate_qr', methods=['POST'])
@@ -14,6 +12,3 @@ def generate_qr():
     qr_img_path = qr_controller.generate_qr(data)
 
     return send_file(qr_img_path, mimetype='image/png')
-
-if __name__ == '__main__':
-    app.run()
